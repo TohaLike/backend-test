0e375a5b40b8d0b58cd4dbea18fbdbb649cba70c
unknown <antonpaper500@gmail.com>
2024-06-26 13:19:45 +0300

    added chat

--- index.js
+++ index.js
@@ -1,82 +1,82 @@
-const mongoose = require("mongoose");
-const Test = require("./models/test.js");
-const Food = require("./models/food.js");
-const bodyParser = require("body-parser");
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const cors = require("cors");
 
-const PORT = 3001;
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-mongoose
-  .connect("mongodb://localhost:27017/admin", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+app.use(
+  cors({
+    origin: "http://localhost:3000",
   })
-  .then(() => console.log("Connected to the database"))
-  .catch((err) => console.log("Could not to connect", err));
+);
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get("/store", async (req, res) => {
+app.get("/chat", (req, res) => {
   try {
-    const foods = await Food.find();
-    res.status(200).json(foods);
-  } catch (err) {
-    res.status(500).json({ message: "error", err: err.message });
+    res.status(200).json("OK");
+  } catch (e) {
+    res.status(500).json({ message: "Error", e: e.message });
   }
 });
 
-app.post("/store", async (req, res) => {
-  try {
-    const data = req.body;
-    const newFood = new Food(data);
-    await newFood.save();
-    res.status(201).json("OK");
-  } catch (err) {
-    res.status(500).json({ message: "error", err: err.message });
-  }
+let connectedUsers = 0;
+
+io.on("connection", (socket) => {
+  // Добавление пользователя в онлайне
+  connectedUsers++;
+
+  // Сообщения
+  socket.on("messages", (room, name, msg) => {
+    socket.to(room).emit("messages", {
+      name: name,
+      message: msg,
+    });
+
+    socket.emit("messages", {
+      name: name,
+      message: msg,
+    });
+
+    console.log(room, name, msg, socket.id);
+  });
+
+  // Поделючение к компнате
+  socket.on("join", (room) => {
+    socket.join(room);
+    io.to(room).emit("users", connectedUsers);
+  });
+
+  // Подключённые пользователи
+  console.log("Total connected users:", connectedUsers);
+
+  // Отключенные пользователи
+  socket.on("disconnect", () => {
+    connectedUsers--;
+    console.log("user disconnected");
+    io.except("some room").emit("hello", connectedUsers);
+  });
+
+  // Транслировать событие пользователям в комнате
+
+  // Транслировать событие пользователям вне комнаты
+  // io.except("some room").emit("hello", connectedUsers);
+
+  // Сообщение чата
+  // socket.on("chat message", (msg) => {
+  //   console.log("message: " + msg);
+  //   io.to("room1").emit("chat message", msg);
+  // });
+
+  socket.leave("some room");
 });
 
-app.post("/store/delete", async (req, res) => {
-  const { id } = req.body;
-  try {
-    const deleteFood = await Food.findByIdAndDelete(id);
-    if (deleteFood) {
-      res.status(200).json({ message: "Deleted OK" });
-    } else {
-      res.status(400).json({ message: "Food not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "error", err: err.message });
-  }
+server.on("error", (error) => {
+  console.error("Socket.IO error:", error);
 });
 
-app.post("/store/update", async (req, res) => {
-  const { id, name } = req.body;
-  try {
-    const updateData = await Food.findByIdAndUpdate(
-      id,
-      { name },
-      { new: true }
-    );
-    if (updateData) {
-      res.status(200).json({ message: "Update" });
-    } else {
-      res.status(400).json({ message: "Not found" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "error", err: err.message });
-  }
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: "404 Not Found" });
-});
-
-// Запуск сервера
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
